b0280aabd4b58fc383ba651071394a1bf3dd3168
root <root@e2e-96-33.ssdcloudindia.net>
2022-03-10 18:24:45 +0530

    added fixed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from rasa_nlu.config import RasaNLUModelConfig
 from rasa_nlu.model import Trainer
 from rasa_nlu import config
 from rasa_nlu.model import Trainer, Metadata, Interpreter
-from simpletransformers.question_answering import QuestionAnsweringModel
+#from simpletransformers.question_answering import QuestionAnsweringModel
 
 import rasa_nlu
 import spacy
