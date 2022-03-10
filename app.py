5f413ac8509f771a14fd8cd1eccd94a50a29662e
GodSaveTheDucks <suhas@claire.ai>
2022-03-10 17:41:33 +0530

    ipadted

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+from rasa_nlu.training_data import load_data
+from rasa_nlu.config import RasaNLUModelConfig
+from rasa_nlu.model import Trainer
+from rasa_nlu import config
+from rasa_nlu.model import Metadata, Interpreter
+
+import rasa_nlu
+import spacy
+import numpy as np
+
+def trainModel(data, model_name):
+    training_data = load_data(data)
+    trainer = Trainer(config.load("config.yml"))
+    trainer.train(training_data)
+    model_directory = trainer.persist('./models/nlu/', fixed_model_name=model_name)
+    interpreter = Interpreter.load(model_directory)
+    return interpreter, model_directory
+
+app = Flask(__name__)
+aug_interpreter, aug_mod_dir = trainModel('models/intent.json','augmented')
+  
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
+
+@app.route('/classifyIntent',methods = ['POST'])
+# ‘/’ URL is bound with hello_world() function.
+def classifyIntent():
+    '''
+    params :  data <obj> - sentence that needs to be classified
+    Classify a sentence based on the intents 
+    '''
+    data = request.json
+    sent = data.get('sent','')
+    res = {}
+    rank = {}
+    if sent:
+        res = aug_interpreter.parse(sent)
+        rank['Intent'] = [res['intent_ranking'][i]['name'] for i in range(len(res['intent_ranking']))]
+        rank['Confidence'] = [str(np.round(res['intent_ranking'][i]['confidence']*100,2))+' %' for i in range(len(res['intent_ranking']))]
+    return rank
+  
+# main driver function
+if __name__ == '__main__':
+  
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run()
